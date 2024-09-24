cb8b628fd5d2c3adcd8c2ba1c5851fc8a3438a62
Heber F Barra <heberferrirabarra@gmail.com>
2024-09-24 17:35:26 -0300

    Update Exercicio01.sql

--- aulas/banco-de-dados/MySQL/exercicios/Exercicio01.sql
+++ aulas/banco-de-dados/MySQL/exercicios/Exercicio01.sql
@@ -1,77 +1,58 @@
-CREATE DATABASE IF NOT EXISTS db_SistemaBancario;
+CREATE DATABASE db_SistemaBancarioHeber;
 
-USE db_SistemaBancario;
+USE db_SistemaBancarioHeber;
 
-CREATE TABLE IF NOT EXISTS tbAgencia (
-	
+CREATE TABLE tbAgencia (
 	cod_Ag INT,
-	nome_Ag VARCHAR(25),
-	cidade_Ag VARCHAR(25),
-	fundos DOUBLE,
-	CONSTRAINT pk_Agencia PRIMARY KEY(cod_Ag),
-	CONSTRAINT un_NOMEtbAgencia UNIQUE (nome_Ag)
-
+    nome_Ag VARCHAR(25) NOT NULL,
+    cidade_Ag VARCHAR(25),
+    fundos DOUBLE NOT NULL,
+    CONSTRAINT pk_tbAgencia PRIMARY KEY(cod_Ag),
+    CONSTRAINT un_NOMEtbAgencia UNIQUE (nome_Ag)
 );
 
-CREATE TABLE IF NOT EXISTS tbCliente (
-
-	cod_Cli INT AUTO_INCREMENT,
-	nome_Cli VARCHAR(25),
-	cidade_Cli VARCHAR(25),
-	rua_Cli VARCHAR(25),
-	CONSTRAINT pk_Cliente PRIMARY KEY(cod_Cli)
-
+CREATE TABLE tbCliente (
+	cod_Cli INT,
+    nome_Cli VARCHAR(25) NOT NULL,
+    cidade_Cli VARCHAR(25) NOT NULL,
+    rua_Cli VARCHAR(35) NOT NULL,
+    CONSTRAINT pk_tbCliente PRIMARY KEY(cod_Cli)
 );
 
-CREATE TABLE IF NOT EXISTS tbConta (
-
-	num_conta INT,
-	cod_Ag INT,
-	saldo DOUBLE,
-	CONSTRAINT pk_Conta PRIMARY KEY(num_conta),
-	CONSTRAINT fk_tbAgenciaConta FOREIGN KEY (cod_Ag)
-	REFERENCES tbAgencia(cod_Ag)
-	ON DELETE CASCADE ON UPDATE CASCADE
-
-);
-
-CREATE TABLE IF NOT EXISTS tbEmprestimo (
-
-	num_Emp INT AUTO_INCREMENT,
-	cod_Ag INT,
-	total DOUBLE,
-	CONSTRAINT pk_Emprestimo PRIMARY KEY (num_Emp),
-	CONSTRAINT fk_tbAgenciatbEmprestimo FOREIGN KEY (cod_Ag)
-	REFERENCES tbAgencia(cod_Ag)
-	ON DELETE CASCADE ON UPDATE CASCADE
-
-);
-
-CREATE TABLE IF NOT EXISTS tbDepositante (
-
+CREATE TABLE tbConta (
 	num_Conta INT,
-	cod_Cliente INT,
-	CONSTRAINT pk_Depositante PRIMARY KEY (num_Conta, cod_Cliente),
-	CONSTRAINT fk_tbContatbDepositante FOREIGN KEY (num_Conta)
-	REFERENCES tbConta(num_Conta)
-	ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT fk_tbClientetbDepositante FOREIGN KEY (cod_Cliente)
-	REFERENCES tbCliente(cod_Cli)
-	ON DELETE CASCADE ON UPDATE CASCADE
-
+    cod_Ag INT,
+    saldo DOUBLE NOT NULL,
+    CONSTRAINT pk_tbConta PRIMARY KEY(num_Conta),
+    CONSTRAINT fk_tbAgenciatbConta FOREIGN KEY (cod_Ag) REFERENCES tbAgencia(cod_Ag)
+    ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS tbDevedor (
-
-	cod_Cliente INT,
+CREATE TABLE tbEmprestimo (
 	num_Emp INT,
-	CONSTRAINT pk_Devedor PRIMARY KEY (cod_Cliente, num_Emp),
-	CONSTRAINT fk_tbClientetbDevedor FOREIGN KEY (cod_Cliente)
-	REFERENCES tbCliente(cod_Cli)
-	ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT fk_tbEmprestimotbDevedor FOREIGN KEY(num_Emp)
-	REFERENCES tbEmprestimo(num_Emp)
-	ON DELETE CASCADE ON UPDATE CASCADE
-
+    cod_Ag INT,
+    total DOUBLE NOT NULL,
+    CONSTRAINT pk_tbEmprestimo PRIMARY KEY (num_Emp),
+    CONSTRAINT fk_tbAgenciatbEmprestimo FOREIGN KEY (cod_Ag) REFERENCES tbAgencia(cod_Ag)
+    ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+CREATE TABLE tbDepositante (
+	num_Conta INT,
+    cod_Cli INT,
+    CONSTRAINT pk_tbDepositante PRIMARY KEY(num_Conta, cod_Cli),
+    CONSTRAINT fk_tbContatbDepositante FOREIGN KEY (num_Conta) REFERENCES tbConta(num_Conta)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_tbClientetbDepositante FOREIGN KEY (cod_Cli) REFERENCES tbCliente(cod_Cli)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE tbDevedor (
+	cod_Cli INT,
+    num_Emp INT,
+    CONSTRAINT pk_tbDevedor PRIMARY KEY(cod_Cli, num_Emp),
+    CONSTRAINT fk_tbClientetbDevedor FOREIGN KEY (cod_Cli) REFERENCES tbCliente(cod_Cli)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_tbEmprestimotbDevedor FOREIGN KEY (num_Emp) REFERENCES tbEmprestimo(num_Emp)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
